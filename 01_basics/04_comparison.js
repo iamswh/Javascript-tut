b632533e5d1ad5155bd13c3bdd5c434fc2bd933d
Wajid <wajidhossain2017@gmail.com>
2023-10-26 13:00:18 +0530

    comparison completed with the explanation

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,22 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+/* All of the aboves are the actual comarison we need to use */
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+/* the above comparison need to avoid these are for learning purpose */
+// ===
+
+console.log("2" === 2); // It strictly checks the value with the datatype also
