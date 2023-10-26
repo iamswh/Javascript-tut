85d3a4ff4671c15b12fba6b00a8664435ffa1220
Wajid <wajidhossain2017@gmail.com>
2023-10-26 10:23:00 +0530

    01_variable complete

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,7 @@
+const accountId = 124485;
+let accoutEmail = "Wajid@gamil.com";
+var accountPassword = "12345";
+accountCity = "Kolkata";
+
+console.log(accountId);
+console.table([accountId, accoutEmail, accountPassword, accountCity]);
