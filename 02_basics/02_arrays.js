0b50a265c6ce976563ca65c5dc62c5e81bf3153a
Wajid <wajidhossain2017@gmail.com>
2023-10-28 19:03:32 +0530

    more array methods covered

--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -17,3 +17,13 @@ const allNewHeros = [...marvelHeros, ...DcHeros];
 const complexArray = [2, 6, [4, 9, 9, [4, 5]]];
 const flatArray = complexArray.flat(Infinity);
 // console.log(flatArray);
+
+// console.log(Array.isArray("Wajid"));
+// console.log(Array.from("Wajid"));
+// console.log(Array.from({ name: "Wajid" })); //interesting
+
+//Array.of() method
+let score1 = 10;
+let score2 = 40;
+let score3 = 50;
+console.log(Array.of(score1, score2, score3)); //Returns a new array from a set of elements.
