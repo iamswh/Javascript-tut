7a887b2510123071b523b4d0b9c09325889d638a
Wajid <wajidhossain2017@gmail.com>
2023-10-28 09:12:46 +0530

    date and time basics

---
+++ 01_basics/07_datesAndTime.js
@@ -0,0 +1,13 @@
+let myDate = new Date();
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+
+let myCreatedDate = new Date(2023, 2, 29);
+// console.log(myCreatedDate.toString());
+// console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now();
+console.log(myTimeStamp);
+console.log(Math.floor(Date.now() / 1000));
