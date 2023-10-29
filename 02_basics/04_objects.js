13330c913d9146a3945296e635c5e49c39e5daf3
Wajid <wajidhossain2017@gmail.com>
2023-10-29 19:19:13 +0530

    objects part 2 complete

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,42 @@
+// const tinderUser = new Object();
+
+const tinderUser = {};
+tinderUser.id = "abc123";
+tinderUser.name = "Wajid";
+tinderUser.isLoggedin = true;
+
+// console.log(tinderUser);
+
+const regularUser = {
+	email: "someone@xmail.com",
+	fullname: {
+		userFullName: {
+			firstName: "Syed Wajid",
+			lastName: "Hossain",
+		},
+	},
+};
+
+console.log(regularUser.fullname.userFullName.firstName);
+
+//Combining two or more objects
+const obj1 = { 1: "a", 2: "b" };
+const obj2 = { 3: "a", 4: "b" };
+const obj3 = { 5: "a", 6: "b" };
+
+// console.log(obj1);
+
+//Using assign method
+// const obj4 = Object.assign({}, obj1, obj2, obj3);
+// console.log(obj4);
+
+//Using spread operator
+const obj4 = { ...obj1, ...obj2, ...obj3 };
+// console.log(obj4);
+
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+console.log(Object.hasOwnProperty("name"));
