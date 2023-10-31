f24370225ada9eea69af6ed673d7f5b6e8e5aaaf
Wajid <wajidhossain2017@gmail.com>
2023-10-31 11:25:38 +0530

    Different types of function declaration

--- 03_basics.js/02_scopes.js
+++ 03_basics.js/02_scopes.js
@@ -21,3 +21,21 @@ if (true) {
 }
 
 // console.log(username);
+
+/* +++++++++++++++++ Different types of function declarationa +++++++++++++++++++ */
+/**process 1: Here you can call the fucntion before accessing it as it is not stored inside any variable */
+console.log(addOne(5));
+
+function addOne(num) {
+	return num + 1;
+}
+
+/**process 2: Here you can not call the fucntion before accessing it as it is stored inside any variable */
+
+// console.log(addTwo(5));  /**You can't use this like that */
+
+const addTwo = function (number) {
+	return number + 2;
+};
+
+console.log(addTwo(5));
