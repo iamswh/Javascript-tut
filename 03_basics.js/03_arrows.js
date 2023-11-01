54ae9f3a1b230a7d335a0759d13b22e984f4ce0c
Wajid <wajidhossain2017@gmail.com>
2023-11-01 16:40:16 +0530

    Arrow functions all basics covered

--- 03_basics.js/03_arrows.js
+++ 03_basics.js/03_arrows.js
@@ -12,4 +12,34 @@ const user = {
 // user.username = "Hossain";
 // user.welcomeMessege();
 
-console.log(this);
+// console.log(this);
+
+/* "This" can not be used here "this" is generally works with objects */
+
+// function chai() {
+// 	let username = "Wajid";
+// 	console.log(this.username);     //o/p: undefined
+// }
+
+// chai();
+
+//Same expression with arrow function
+const chai = () => {
+	let username = "Wajid";
+	console.log(this.username);
+};
+// chai();
+
+/* let's deep dive into arrow function */
+// const addTwo = (num1, num2) => {
+// 	return num1 + num2;
+// };
+
+//Implicit return of arrow functoin
+// const addTwo = (num1, num2) => num1 + num2;
+// const addTwo = (num1, num2) => (num1 + num2);    //This is basically used in react
+// console.log(addTwo(4, 5));
+
+//Returning objects in arrow functions
+const objectReturn = () => ({ name: "Wajid Hossain" });
+console.log(objectReturn());
