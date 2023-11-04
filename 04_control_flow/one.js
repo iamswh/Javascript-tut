d97a56a1864bafd72961f61f5edae8728ae43d16
Wajid <wajidhossain2017@gmail.com>
2023-11-04 11:00:20 +0530

    basics of control flow

---
+++ 04_control_flow/one.js
@@ -0,0 +1,17 @@
+//if
+// const isUserloggedIn = true;
+
+// const temparature = 41;
+// if (temparature === 40) {
+// 	console.log("less than 50");
+// } else {
+// 	console.log("greater than 50");
+// }
+
+const score = 200;
+if (score > 100) {
+	let power = "fly";
+	console.log(`User power: ${power}`);
+}
+
+// console.log(`User power: ${power}`); //This will give an error cause it is outside the scope
