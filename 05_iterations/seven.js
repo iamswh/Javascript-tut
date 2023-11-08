4448410c0f1a206eee32fd1848a1070d19eb3778
Wajid <wajidhossain2017@gmail.com>
2023-11-08 23:53:42 +0530

    .map and chaining compete

--- 05_iterations/seven.js
+++ 05_iterations/seven.js
@@ -5,4 +5,18 @@ const myNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 // const newNums = myNumbers.map((num) => {
 // 	return num + 10;
 // });
+// console.log(newNums);
+
+//Chain-ing of different methods
+const newNums = myNumbers
+	.map((num) => {
+		return num + 10;
+	})
+	.map((num) => {
+		return num + 1;
+	})
+	.filter((num) => {
+		return num > 15;
+	});
+
 console.log(newNums);
