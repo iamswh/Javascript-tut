5661a36ac0bdc349bde4bd53a92e11ae6ac7fbbc
Wajid <wajidhossain2017@gmail.com>
2023-11-29 12:25:51 +0530

    solution 1 JS file

---
+++ 07_projects/solution1.js
@@ -0,0 +1,21 @@
+// JS file of color changer project
+
+const buttons = document.querySelectorAll(".button");
+const body = document.querySelector("body");
+
+buttons.forEach((eachButton) => {
+	eachButton.addEventListener("click", (event) => {
+		if (event.target.id === "grey") {
+			body.style.backgroundColor = event.target.id;
+		}
+		if (event.target.id === "white") {
+			body.style.backgroundColor = event.target.id;
+		}
+		if (event.target.id === "blue") {
+			body.style.backgroundColor = event.target.id;
+		}
+		if (event.target.id === "yellow") {
+			body.style.backgroundColor = event.target.id;
+		}
+	});
+});
