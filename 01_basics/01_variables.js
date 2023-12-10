3d6ab7db9a9095802a6717d803a814a26e57083f
Syed Wajid Hossain <wajidhossain2017@gmail.com>
2023-12-10 17:41:10 +0530

    type added

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,7 +1,7 @@
 const accountId = 124485;
 let accoutEmail = "Wajid@gamil.com";
 var accountPassword = "12345";
-accountCity = "Kolkata";
+const accountCity = "Kolkata";
 
 console.log(accountId);
 console.table([accountId, accoutEmail, accountPassword, accountCity]);
